fbdb1c2504fb898fa2b0ae66058a34dc58e6d4d1
programer1983 <programer972@gmail.com>
2023-04-02 11:50:21 +0300

    create reviews  block and slider

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const reviewsSwiper = document.querySelector(".swiper-reviews")
+
+if (reviewsSwiper){
+    const swiper = new Swiper('.swiper-reviews', {
+        autoHeight: true,
+        loop: true,
+      
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        }
+      })
+}
+
+
+
+
